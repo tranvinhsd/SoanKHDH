3185408a51c254217ff0ad7bd241b79ceaa0c1b7
tranvinhsd <tranvinhsd@gmail.com>
2026-09-20 21:27:45 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/Soan_KHDH/',
+    base: '/SoanKHDH/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
